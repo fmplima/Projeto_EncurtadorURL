ec4b4d53f7f3de278bedfea83d9686ffea8110e3
Felipe Lima <felipelima@Felipes-MacBook-Pro-2.local>
2019-02-14 02:29:36 -0200

    first commit

---
+++ projectJobUol/src/main/java/br/com/uol/shorten/v1/resource/ShortenServiceResource.java
@@ -0,0 +1,10 @@
+package br.com.uol.shorten.v1.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/url")
+public class ShortenServiceResource {
+	
+	
+
+}
